64ef36f03b1efd682a03faa6b44001bccb4d93c0
Tiago Wanke Marques <tiagowanke@gmail.com>
2018-04-10 10:24:58 +0200

    Updated MarsTest to have its own MAX_X and MAX_Y to tests instead of getting this information from Mars class. This way it will allways check the Mars' edges correctly even if the Mars implementation were changed.

--- src/test/java/robots/model/MarsTest.java
+++ src/test/java/robots/model/MarsTest.java
@@ -7,53 +7,40 @@ import org.junit.Test;
 
 public class MarsTest {
 
+    private static final short MAX_X = 4;
+    private static final short MAX_Y = 4;
+
     @Test
     public void canMoveMustReturnFalse() {
 
         final Mars mars = new Mars();
-        int x, y;
 
         // x direction greater than allowed
-        x = mars.getMaxX() + 1;
-        y = 0;
-        assertFalse(mars.canMove(x, y));
+        assertFalse(mars.canMove(MAX_X + 1, 0));
 
         // x direction lower than allowed
-        x = -1;
-        y = 0;
-        assertFalse(mars.canMove(x, y));
+        assertFalse(mars.canMove(-1, 0));
 
         // y direction greated than allowed
-        x = 0;
-        y = mars.getMaxY() + 1;
-        assertFalse(mars.canMove(x, y));
+        assertFalse(mars.canMove(0, MAX_Y + 1));
 
         // y direction lower than allowed
-        x = 0;
-        y = -1;
-        assertFalse(mars.canMove(x, y));
+        assertFalse(mars.canMove(0, -1));
     }
 
     @Test
     public void canMoveMustReturnTrue() {
 
         final Mars mars = new Mars();
-        int x, y;
 
         // x and y direction on lower limit
-        x = 0;
-        y = 0;
-        assertTrue(mars.canMove(x, y));
+        assertTrue(mars.canMove(0, 0));
 
         // x direction on highter limit
-        x = mars.getMaxX();
-        y = 0;
-        assertTrue(mars.canMove(x, y));
+        assertTrue(mars.canMove(MAX_X, 0));
 
         // y direction on highter limit
-        x = 0;
-        y = mars.getMaxY();
-        assertTrue(mars.canMove(x, y));
+        assertTrue(mars.canMove(0, MAX_Y));
 
     }
 
